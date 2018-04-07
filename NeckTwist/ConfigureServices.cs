80008c3da099d33efd040014fa0e9bc8b9bf316a
Jonathan Stendahl <jonathan.stendahl@live.se>
2018-04-07 22:22:56 +0200

    Adding example ui to test identity server auth

--- NeckTwist/ConfigureServices.cs
+++
@@ -1,9 +0,0 @@
-
-
-
-services.AddIdentityServer()
-.AddInMemoryClients(new List<Client>())
-    .AddInMemoryIdentityResources(new List<IdentityResource>())
-    .AddInMemoryApiResources(new List<ApiResource>())
-    .AddTestUsers(new List<TestUser>())
-    .AddDeveloperSigningCredential();
--- NeckTwist/Startup.cs
+++ NeckTwist/Startup.cs
@@ -1,36 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace NeckTwist
-{
-    public class Startup
-    {
+namespace NeckTwist {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddIdentityServer ()
+                .AddInMemoryClients (Clients.Get ())
+                .AddInMemoryIdentityResources (Resources.GetIdentityResources())
+                .AddInMemoryApiResources (Resources.GetApiResources())
+                .AddTestUsers (Users.Get())
+                .AddDeveloperSigningCredential();
+
+            services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles ();
+            app.UseIdentityServer ();
+            app.UseMvcWithDefaultRoute ();
         }
     }
 }
