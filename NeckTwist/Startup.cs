400066cce067a639775347c47b85b126c7fc1533
Jonathan Stendahl <jonathan.stendahl@live.se>
2018-04-07 15:27:12 +0200

    Moved folder and hoocked up angular to wwwroot and added npm build in csproj

--- NeckTwist/Startup.cs
+++ NeckTwist/Startup.cs
@@ -24,6 +24,8 @@ namespace NeckTwist
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
